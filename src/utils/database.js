4a17cf565d94c6b52c0793c017b7531e3809b1ec
Jose Luis <zonajl@gmail.com>
2023-03-23 19:16:17 -0400

    add config

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,13 +1,14 @@
 const {Sequelize} = require('sequelize');
+const config = require('../../config')
 require('dotenv').config()
 
 const db = new Sequelize({
     dialect:'postgres',
-    host:'localhost',
-    port:'5432',
-    username:'postgres',
-    password:'Lucianita1*',
-    database:'crudUsersLocal',
+    host: config.db.host,
+    port: config.db.port,
+    username: config.db.user,
+    password: config.db.pass,
+    database: config.db.name,
     dialectOptions: 
         process.env.NODE_ENV === 'production'
         ? {
